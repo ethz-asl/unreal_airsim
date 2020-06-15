0e84a257461bddb0a113de72b79dd0ff11990478
Schmluk <schmluk@student.ethz.ch>
2020-06-15 14:53:57 +0200

    fix IR correction segv

--- src/simulator_processing/infrared_id_compensation.cpp
+++ src/simulator_processing/infrared_id_compensation.cpp
@@ -15,33 +15,31 @@ ProcessorFactory::Registration<InfraredIdCompensation>InfraredIdCompensation::re
 
 bool InfraredIdCompensation::setupFromRos(const ros::NodeHandle &nh, const std::string &ns) {
   nh_ = nh;
-  if (!nh_.hasParam("input_topic")){
+  if (!nh_.hasParam(ns + "input_topic")) {
     LOG(ERROR) << "InfraredIdCompensation requires the 'input_topic' to be set.";
     return false;
   }
-  if (!nh_.hasParam("output_topic")){
+  if (!nh_.hasParam(ns + "output_topic")) {
     LOG(ERROR) << "InfraredIdCompensation requires the 'output_topic' to be set.";
     return false;
   }
   std::string input_topic, output_topic;
-  nh_.param("input_topic", input_topic);
-  nh_.param("output_topic", output_topic);
+  nh_.getParam(ns + "input_topic", input_topic);
+  nh_.getParam(ns + "output_topic", output_topic);
   pub_ = nh_.advertise<sensor_msgs::Image>(output_topic, 10);
   sub_ = nh_.subscribe(input_topic, 10, &InfraredIdCompensation::imageCallback, this);
   return true;
 }
 
 void InfraredIdCompensation::imageCallback(const sensor_msgs::ImagePtr &msg) {
-  cv_bridge::CvImageConstPtr img = cv_bridge::toCvShare(msg, msg->encoding);
-  cv::Mat out(img->image.rows, img->image.cols, CV_8UC1);
+  cv_bridge::CvImagePtr img = cv_bridge::toCvCopy(msg, msg->encoding);
   for (int v = 0; v < img->image.rows; v++) {
     for (int u = 0; u < img->image.cols; u++) {
-      cv::Vec3b seg = img->image.at<cv::Vec3b>(v, u);
-      out.at<cv::Scalar>(v, u) = infrared_compensation_[seg[0]];
+      auto &seg = img->image.at<cv::Vec3b>(v, u);
+      seg[0] = infrared_compensation_[seg[0]];
     }
   }
-  sensor_msgs::ImagePtr out_msg = cv_bridge::CvImage(msg->header, "CV_8UC1", out).toImageMsg();
-  pub_.publish(out_msg);
+  pub_.publish(img->toImageMsg());
 }
 
 } // namespcae unreal_airsim::simulator_processor
