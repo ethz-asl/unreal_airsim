15079fa171c18ac04d42512977dba7e310928cf1
René Zurbrügg <zrene@student.ethz.ch>
2021-04-14 08:47:35 +0200

    Moved code to load camera infos outside of if statement

--- src/online_simulator/simulator.cpp
+++ src/online_simulator/simulator.cpp
@@ -42,6 +42,7 @@ AirsimSimulator::AirsimSimulator(const ros::NodeHandle& nh,
           OdometryDriftSimulator::Config::fromRosParams(nh_private)) {
   // configure
   readParamsFromRos();
+std::cout << "Hi " << std::endl;
 
   // airsim
   bool success = setupAirsim();
@@ -300,6 +301,16 @@ bool AirsimSimulator::setupROS() {
       timer = sensor_timers_.back().get();
     }
     timer->addSensor(*this, i);
+
+    // Save camera params (e.g. FOV) as they are needed to generate pointcloud
+    if (config_.sensors[i]->sensor_type == Config::Sensor::TYPE_CAMERA) {
+      auto camera = (Config::Camera*)config_.sensors[i].get();
+      // This assumes the camera exists, which should always be the case with
+      // the auto-generated-config.
+      camera->camera_info = airsim_move_client_.simGetCameraInfo(
+              camera->name, config_.vehicle_name);
+     }
+
     if (!config_.publish_sensor_transforms) {
       // Broadcast all sensor mounting transforms via static tf.
       geometry_msgs::TransformStamped static_transformStamped;
@@ -307,11 +318,6 @@ bool AirsimSimulator::setupROS() {
       if (config_.sensors[i]->sensor_type == Config::Sensor::TYPE_CAMERA) {
         // Camera frames are x right, y down, z depth
         rotation = Eigen::Quaterniond(0.5, -0.5, 0.5, -0.5) * rotation;
-        auto camera = (Config::Camera*)config_.sensors[i].get();
-        // This assumes the camera exists, which should always be the case with
-        // the auto-generated-config.
-        camera->camera_info = airsim_move_client_.simGetCameraInfo(
-            camera->name, config_.vehicle_name);
         // TODO(Schmluk): Might want to also publish the camera info or convert
         // to intrinsics etc
       }
