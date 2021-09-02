474866903d2beedddd000a52e97bd94343dc4ffe
mansoor <mansoor.cheema@tum.de>
2021-09-02 20:01:55 +0200

    remove extra line

--- src/simulator_processing/infrared_id_compensation.cpp
+++ src/simulator_processing/infrared_id_compensation.cpp
@@ -56,7 +56,6 @@ bool InfraredIdCompensation::setupFromRos(const ros::NodeHandle& nh,
 void InfraredIdCompensation::imageCallback(const sensor_msgs::ImagePtr& msg) {
   try {
     cv_bridge::CvImagePtr img = cv_bridge::toCvCopy(msg, msg->encoding);
-
     for (int v = 0; v < img->image.rows; v++) {
       for (int u = 0; u < img->image.cols; u++) {
         img->image.at<uchar>(v, u) =
